a594ea2cf001901c4010b5b958dd7bbd92165857
Kay Z <l04m33@gmail.com>
2017-08-22 14:33:16 +0800

    Vanilla Android app project created by "android create project", using Gradle 1.12 and Android plugin 2.3.3

---
+++ src/main/java/com/theerrorlog/superbapp/SuperbAppActivity.java
@@ -0,0 +1,15 @@
+package com.theerrorlog.superbapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SuperbAppActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
