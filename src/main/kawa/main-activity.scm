89a05bcd38a0bb731b90e32ff9bbd49c970677f1
Kay Z <l04m33@gmail.com>
2017-08-24 10:47:46 +0800

    The Override annotation

--- src/main/kawa/main-activity.scm
+++ src/main/kawa/main-activity.scm
@@ -1,5 +1,6 @@
 (define-library (com theerrorlog superbapp main-activity)
   (import (kawa base)
+          (class java.lang Override)
           (class android.app Activity)
           (class android.os Bundle))
   (export MainActivity)
@@ -7,5 +8,6 @@
   (begin
     (define-simple-class MainActivity (Activity)
       ((onCreate (savedInstanceState :: Bundle)) :: void
+          (@Override)
         (invoke-special Activity (this) 'onCreate savedInstanceState)
         ((this):setContentView (static-field R:layout 'main))))))
